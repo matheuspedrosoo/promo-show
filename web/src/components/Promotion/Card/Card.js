e661e5499acb6fc73bace38b99615063b69543f4
matheussbs <76970330+matheussbs@users.noreply.github.com>
2021-03-05 11:09:37 -0300

    implemented update to promotion form

--- web/src/components/Promotion/Card/Card.js
+++ web/src/components/Promotion/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Card.css'
 
 const PromotionCard = ({ promotion }) => (
@@ -30,6 +31,7 @@ const PromotionCard = ({ promotion }) => (
         >
           IR PARA O SITE
         </a>
+        <Link to={`/edit/${promotion.id}`}>Editar</Link>
       </footer>
     </div>
   </div>
